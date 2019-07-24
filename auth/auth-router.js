ba1d137bef431a7b6908e14364ea7f7bb3be0b0f
Shola Ayeni <ayenisholah@yahoo.com>
2019-07-24 14:05:33 +0100

    auth login endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
@@ -12,6 +13,28 @@ router.post('/register', (req, res) => {
   Users.add(user)
     .then(saved => {
       res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        res.status(200).json({
+          message: `Welcome ${user.username} !`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
     })
     .catch(error => {
       res.status(500).json(error);
