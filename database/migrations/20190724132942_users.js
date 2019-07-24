683cb23e84e74d73a17dad050476c61b1fc2b42c
Shola Ayeni <ayenisholah@yahoo.com>
2019-07-24 13:30:29 +0100

    Database schema

---
+++ database/migrations/20190724132942_users.js
@@ -0,0 +1,22 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('users', table => {
+    table.increments();
+
+    table
+      .string('username', 128)
+      .notNullable()
+      .unique();
+
+    table
+      .string('passwords', 128)
+      .notNullable();
+
+    table
+      .string('departments', 128)
+      .notNullable();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('users');
+};
